5187d0d223cb112bd8f04e480ef8f8e6c1cac36c
korzh17 <korzh17121996@gmail.com>
2020-01-12 18:55:45 +0100

    Exception для export/pdf "document has no pages".

---
+++ src/main/java/ru/animal/world/controller/errors/ErrorResponse.java
@@ -0,0 +1,14 @@
+package ru.animal.world.controller.errors;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+    private String error;
+    private String message;
+
+    public ErrorResponse(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+}
